53c34113ad855fbb974cc314b7267fb5e9d815da
Alexey Bannov <alexey.bannov@onlyoffice.com>
2019-01-28 18:14:21 +0300

    Revert "blockchain: one password for all document versions"
    This reverts commit 88f74437f0e70f3fe77a66bc367b814d76bf4d22.

--- ChromiumBasedEditors/plugins/encrypt/ui/engine/blockchain/{B17BDC61-59FC-41A7-A471-CD2C415A665E}/code.js
+++ ChromiumBasedEditors/plugins/encrypt/ui/engine/blockchain/{B17BDC61-59FC-41A7-A471-CD2C415A665E}/code.js
@@ -15,20 +15,6 @@
 		}
 		return hex;
 	}
-
-	function byteArraytoHexString(byteArray) {
-		return Array.from(byteArray, function(byte) {
-		  return ('0' + (byte & 0xFF).toString(16)).slice(-2);
-		}).join('')
-	}
-
-	
-	function _guidsAreEqual(left, right) {
-		var rgx = /[\{\-\}]/g;
-		var txtLeft = left.replace(rgx, '').toUpperCase();
-		var txtRight = right.replace(rgx, '').toUpperCase();
-		return txtLeft === txtRight;
-	};
 	
 	window.onSystemMessage = function(obj)
 	{	
@@ -40,30 +26,13 @@
 			{
 				case "generatePassword":
 				{
-					let _pass = "";
+					let _pass = _generatePassword();
 
 					if (!isConnected)
 						_pass = "";
 
-					let docinfo = obj.docinfo;
-
-					let nonce = nacl.util.encodeBase64(nacl.randomBytes(nacl.box.nonceLength));
+					let docinfo =  nacl.util.encodeBase64(nacl.randomBytes(nacl.box.nonceLength));
 					
-					if (!docinfo) {
-						_pass = _generatePassword();
-
-						let documentRandomHash256 = byteArraytoHexString(nacl.randomBytes(32));
-
-						docinfo = documentRandomHash256 + "|" + nonce;
-					}
-					else
-					{
-						let _documentHash256 = obj.docinfo.split("|")[0];
-
-						_pass = ONLYONET.getFilePassword("0x" + _documentHash256);
-						docinfo = _documentHash256 + "|" + nonce;
-					}
-
 					AscDesktopEditor.sendSystemMessage({ type : "generatePassword", password : _pass,  docinfo : docinfo });
 					
 					break;					
@@ -78,7 +47,10 @@
 						break;
 					}
 	
-					let _hashAsHex = "0x" + obj.docinfo.split("|")[0];
+					let _hashAsHex = "0x" + _convertToHex(atob(obj.hash)); 
+
+			 		fileHash = _hashAsHex;
+				
 					let _pass = ONLYONET.getFilePassword(_hashAsHex);
 
 					if (null == _pass) _pass = "";
@@ -103,19 +75,10 @@
 					}
 					else
 					{
-						let _hashAsHex = "0x" + obj.docinfo.split("|")[0];
+						let _hashAsHex = "0x" + _convertToHex(atob(obj.hash));
 
-						let _passByHash	= ONLYONET.getFilePassword(_hashAsHex);
-
-						if (_passByHash) {
-							if (!_guidsAreEqual(_passByHash, obj.password))
-								obj.error = "Password with this hash already exist in blockchain";
-		
-							AscDesktopEditor.sendSystemMessage(obj);
-		
-							break;
-						}
-						
+						fileHash = _hashAsHex;
+	
 						let _pass = encodeURIComponent(obj.password);				
 				
 						ONLYONET.saveFilePassword(_hashAsHex, _pass).then(result => {							
@@ -138,11 +101,9 @@
 						break;
 					}	
 
-					let nonce = obj.docinfo.split("|")[1];
-
 					for (var i = 0; i < obj.data.length; i++)
 					{
-						obj.data[i] = ONLYONET.encryptData(obj.data[i], obj.password, nonce);
+						obj.data[i] = ONLYONET.encryptData(obj.data[i], obj.password, obj.docinfo);
 					}	
 					
 					AscDesktopEditor.sendSystemMessage(obj);
@@ -157,17 +118,15 @@
 						break;
 					}				
 
-					let nonce = obj.docinfo.split("|")[1];
-
 					for (var i = 0; i < obj.data.length; i++)
 					{
 						if (obj.data[i]["change"])
 						{
-							obj.data[i]["change"] = ONLYONET.decryptData(obj.data[i]["change"], obj.password, nonce);
+							obj.data[i]["change"] = ONLYONET.decryptData(obj.data[i]["change"], obj.password, obj.docinfo );
 						}
 						else
 						{
-							obj.data[i] = ONLYONET.decryptData(obj.data[i], obj.password, nonce);
+							obj.data[i] = ONLYONET.decryptData(obj.data[i], obj.password, obj.docinfo);
 						}
 					}	
 					
@@ -187,7 +146,7 @@
 				}
 				case "share":
 				{					
-					let _hashAsHex = "0x" + obj.docinfo.split("|")[0];
+					let _hashAsHex = "0x" + _convertToHex(atob(obj.hash));
 
 					let _pass = ONLYONET.getFilePassword(_hashAsHex);
 					
