e879ff277771ccaa906ff40c1692864912015367
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-02-26 12:50:20 +0300

    Refactoring add video/audio

--- ChromiumBasedEditors/plugins/{060E6A7D-2766-44E8-A0EE-9A8CB9DB00D1}/code.js
+++ ChromiumBasedEditors/plugins/{060E6A7D-2766-44E8-A0EE-9A8CB9DB00D1}/code.js
@@ -2,43 +2,9 @@
 
 	window.Asc.plugin.init = function(text)
 	{
-		if (!window.Asc.plugin.translateManager)
-		{
-			setTimeout(function(){
-				window.Asc.plugin.init();
-			}, 100);
-			return;
-		}
-		
-		var filter = window.Asc.plugin.tr("Audio Files") + " (*.flac *.mp3 *.ogg *.wav *.wma *.ape *.aac *.m4a *.alac);";
-		filter += (";" + window.Asc.plugin.tr("All Files") + " (*.*)");
-		
-	    window["AscDesktopEditor"]["OpenFilenameDialog"](filter, false, function(_file) {
-			var file = _file;
-			if (Array.isArray(file))
-				file = file[0];
-			
-			if (!file)
-			{
-				window.Asc.plugin.executeCommand("close", "");
-				return;
-			}
-			
-			window.Asc.plugin.executeMethod("StartAction", ["Block", window.Asc.plugin.tr("Copying audio...")], function() {
-				
-				setTimeout(function(){
-					window["AscDesktopEditor"]["AddAudio"](file, window.Asc.plugin.guid.substr(4));
-					window.Asc.plugin.executeMethod("EndAction", ["Block", window.Asc.plugin.tr("Copying audio...")]);
-					window.Asc.plugin.executeCommand("close", "");
-				}, 100);
-				
-			});
+		window.Asc.plugin.executeMethod("AddAudio", [], function() {
+			this.executeCommand("close", "");
 		});
-	};
-	
-	window.Asc.plugin.button = function(id)
-	{
-		this.executeCommand("close", "");
 	};
 	
 })(window, undefined);
