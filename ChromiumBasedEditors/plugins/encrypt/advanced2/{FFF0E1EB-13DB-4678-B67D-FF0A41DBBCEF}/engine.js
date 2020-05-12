14058b548852b95a57bf5f12ac615e1a18ab7758
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-05-12 18:03:24 +0300

    .

--- ChromiumBasedEditors/plugins/encrypt/advanced2/{FFF0E1EB-13DB-4678-B67D-FF0A41DBBCEF}/engine.js
+++ ChromiumBasedEditors/plugins/encrypt/advanced2/{FFF0E1EB-13DB-4678-B67D-FF0A41DBBCEF}/engine.js
@@ -1,49 +1,98 @@
 (function(window, undefined){
 	
-	window.engine = {};
-
-	function generate_password_guid_random()
+	window.DocInfoWorker = {};
+	window.DocInfoWorker.isUseOnePasswordOnAllVersions = false;
+	window.DocInfoWorker.TmpPassword = "";
+	window.DocInfoWorker.User = null;
+	
+	// work with docinfo
+	// create from passwords
+	window.DocInfoWorker.generateDocInfo = function(users, password)
 	{
+		var result = "";
+		for (var i = 0, len = users.length; i < len; i++)
+		{
+			if (users[i].publicKey)
+			{
+				result += (users[i].userId + "\n");
+				result += (window.AscDesktopEditor.CryproRSA_EncryptPublic(users[i].publicKey, password) + "\n\n");
+			}				
+		}
+		return result;
+	};
+	// decrypt password from docinfo
+	window.DocInfoWorker.readPassword = function(docinfo)
+	{
+		var user = window.DocInfoWorker.User[2];
+		var index = docinfo.indexOf(user);
+		
+		if (-1 == index)
+			return "";
+		
+		var delimeter = "<!--break-->";
+		var start = docinfo.indexOf(delimeter, index);
+		if (-1 == index)
+			return "";
+		
+		var end = docinfo.indexOf(delimeter, start + 1);
+		if (-1 == end)
+			return "";
+		
+		var encPassword = docinfo.substring(start + delimeter.length, end);
+		return window.AscDesktopEditor.CryproRSA_DecryptPrivate(window.DocInfoWorker.User[0], encPassword);
+	};
+	
+	// генерация нового пароля
+	window.DocInfoWorker.createPasswordNew = function() {
 		function s4() {
 			return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 		}
 		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 	}
-
-	function generate_password() 
+	// запрос на новый пароль
+	window.DocInfoWorker.createPassword = function()
 	{
-		if (!window.engine.user)
-			return { pass : "", docinfo : "" };
-		
-		var ret = { pass : generate_password_guid_random(), docinfo : "" };
-		
-		// TODO: all keys
-		ret.docinfo = window.AscDesktopEditor.CryproRSA_EncryptPublic(window.engine.user[1], ret.pass);
-		
-		return ret;
-	}
+		var password = window.Asc.plugin.documentPassword;
+		if (null == password || "" == password || true !== this.isUseOnePasswordOnAllVersions)
+			password = window.DocInfoWorker.createPasswordNew();
+		return password;
+	};
 	
+	DocInfoWorker.generatePassword = function()
+	{
+		this.TmpPassword = this.createPassword();
+				
+		window.AscDesktopEditor.cloudCryptoCommandMainFrame({ name: "window.cloudCryptoCommand", type: "getsharingkeys" }, function(obj){
+			
+			window.Asc.plugin.onEngineSystemMessage({ 
+				type : "generatePassword", 
+				password : window.DocInfoWorker.TmpPassword, 
+				docinfo : window.DocInfoWorker.generateDocInfo(obj.keys, window.DocInfoWorker.TmpPassword)
+			});
+			
+		});
+	};
+	
+	window.engine = {};
+
 	window.engine.sendSystemMessage = function(obj)
 	{
-		if (!window.engine.user)
-			window.engine.user = window.AscDesktopEditor.CryptoCloud_GetUserInfo();
+		if (!window.DocInfoWorker.User)
+			window.DocInfoWorker.User = window.AscDesktopEditor.CryptoCloud_GetUserInfo();
 		
 		switch (obj.type)
 		{
 			case "generatePassword":
 			{
-				var _pass = generate_password();
-				window.Asc.plugin.onEngineSystemMessage({ type : "generatePassword", password : _pass.pass, docinfo : _pass.docinfo });
+				window.DocInfoWorker.generatePassword();
 				break;
 			}
 			case "getPasswordByFile":
 			{
-				var encPasswordForUser = obj.docinfo; // TODO: parse for user
-				var _pass = "";
-				if (window.engine.user)
-					_pass = window.AscDesktopEditor.CryproRSA_DecryptPrivate(window.engine.user[0], encPasswordForUser);
-				
-				window.Asc.plugin.onEngineSystemMessage({ type : "getPasswordByFile", password : _pass });
+				window.Asc.plugin.onEngineSystemMessage({ 
+					type : "getPasswordByFile", 
+					password : window.DocInfoWorker.readPassword(obj.docinfo)
+				});
  				break;
 			}
 			case "setPasswordByFile":
@@ -55,8 +104,6 @@
 				}
 				else
 				{
-					// saving...
-					
 					// send message on end
 					setTimeout(function() {
 						// timer only for visibility saving message (delay)
