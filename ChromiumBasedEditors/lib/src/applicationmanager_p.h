e359c932cf9bea68df17045e1bfffbf49d21056a
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2021-01-20 16:18:27 +0300

    Support working with service workers (caching AllFonts.js) Add "crypted" flag to recents

--- ChromiumBasedEditors/lib/src/applicationmanager_p.h
+++ ChromiumBasedEditors/lib/src/applicationmanager_p.h
@@ -826,6 +826,7 @@ public:
     std::wstring    m_sUrl;
     std::wstring    m_sExternalCloudId;
     std::wstring    m_sParentUrl;
+    bool            m_bIsCrypted;
 
 public:
     CAscEditorFileInfo()
@@ -834,6 +835,7 @@ public:
         m_nId = -1;
         m_nFileType = 0;
         m_bIsViewer = false;
+        m_bIsCrypted = false;
     }
 
     void UpdateDate()
@@ -1550,6 +1552,7 @@ public:
                 oInfo.m_sUrl = oFile.GetAttribute(L"url");
                 oInfo.m_sExternalCloudId = oFile.GetAttribute(L"externalcloud");
                 oInfo.m_sParentUrl = oFile.GetAttribute(L"parent");
+                oInfo.m_bIsCrypted = (oFile.GetAttribute(L"crypted") == L"1") ? true : false;
                 m_arRecents.push_back(oInfo);
 
                 map_files.insert(std::pair<std::wstring, bool>(sPath, true));
@@ -1558,7 +1561,7 @@ public:
 
         Recents_Dump(false);
     }
-    void Recents_Add(const std::wstring& sPathSrc, const int& nType, const std::wstring& sUrl = L"", const std::wstring& sExternalCloudId = L"", const std::wstring& sParentUrl = L"")
+    void Recents_Add(const std::wstring& sPathSrc, const int& nType, const std::wstring& sUrl = L"", const std::wstring& sExternalCloudId = L"", const std::wstring& sParentUrl = L"", bool bIsCrypted = false)
     {
         CTemporaryCS oCS(&m_oCS_LocalFiles);
 
@@ -1587,6 +1590,7 @@ public:
         oInfo.m_sUrl = sUrl;
         oInfo.m_sExternalCloudId = sExternalCloudId;
         oInfo.m_sParentUrl = sParentUrl;
+        oInfo.m_bIsCrypted = bIsCrypted;
         oInfo.UpdateDate();
 
         oInfo.m_nFileType = nType;
@@ -1674,6 +1678,11 @@ public:
                 oBuilder.WriteEncodeXmlString(i->m_sParentUrl);
             }
 
+            if (i->m_bIsCrypted)
+            {
+                oBuilder.WriteString(L"\" crypted=\"1");
+            }
+
             oBuilder.WriteString(L"\" />");
         }
         oBuilder.WriteString(L"</recents>");
@@ -1705,6 +1714,8 @@ public:
             oBuilder.AddInt(i->m_nId);
             oBuilder.WriteString(L",type:");
             oBuilder.AddInt(i->m_nFileType);
+            oBuilder.WriteString(L",crypted:");
+            oBuilder.AddInt(i->m_bIsCrypted ? 1 : 0);
             oBuilder.WriteString(L",path:\"");
             oBuilder.WriteEncodeXmlString(i->m_sPath);
             oBuilder.WriteString(L"\",modifyed:\"");
