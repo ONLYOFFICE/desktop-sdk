ec454c5c80505dc8fe25f6cff3e88c9f7a8bf6c8
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2023-06-16 10:11:20 +0300

    Adaptation for Qt6

--- ChromiumBasedEditors/lib/qt_wrapper/src/qdpichecker.cpp
+++ ChromiumBasedEditors/lib/qt_wrapper/src/qdpichecker.cpp
@@ -41,7 +41,11 @@
 #endif
 
 #ifdef _LINUX
-#include <QX11Info>
+# if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+#  include <QX11Info>
+# else
+#  include <QtGui/private/qtx11extras_p.h>
+# endif
 #endif
 
 QDpiChecker::QDpiChecker(CAscApplicationManager* pManager) : CAscDpiChecker(pManager)
