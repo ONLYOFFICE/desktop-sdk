95a0050c717689c7d809829b7e224ab38e7a14c6
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2023-06-23 16:52:20 +0300

    Fix work with certificates with \\ \" symbols in info

--- ChromiumBasedEditors/lib/src/utils.h
+++ ChromiumBasedEditors/lib/src/utils.h
@@ -49,97 +49,97 @@
 
 namespace NSCommon
 {
-    static void makeUpper(std::string& url)
-    {
-        int nLen = (int)url.length();
-        char* pStr = (char*)url.c_str();
+	static void makeUpper(std::string& url)
+	{
+		int nLen = (int)url.length();
+		char* pStr = (char*)url.c_str();
 
-        for (int i = 0; i < nLen; ++i)
-        {
-            if (pStr[i] >= 'a' && pStr[i] <= 'z')
-                pStr[i] = pStr[i] + 'A' - 'a';
-        }
-    }
-    static void makeUpperW(std::wstring& url)
-    {
-        int nLen = (int)url.length();
-        wchar_t* pStr = (wchar_t*)url.c_str();
+		for (int i = 0; i < nLen; ++i)
+		{
+			if (pStr[i] >= 'a' && pStr[i] <= 'z')
+				pStr[i] = pStr[i] + 'A' - 'a';
+		}
+	}
+	static void makeUpperW(std::wstring& url)
+	{
+		int nLen = (int)url.length();
+		wchar_t* pStr = (wchar_t*)url.c_str();
 
-        for (int i = 0; i < nLen; ++i)
-        {
-            if (pStr[i] >= 'a' && pStr[i] <= 'z')
-                pStr[i] = pStr[i] + 'A' - 'a';
-        }
-    }
-    static void makeLower(std::string& url)
-    {
-        int nLen = (int)url.length();
-        char* pStr = (char*)url.c_str();
+		for (int i = 0; i < nLen; ++i)
+		{
+			if (pStr[i] >= 'a' && pStr[i] <= 'z')
+				pStr[i] = pStr[i] + 'A' - 'a';
+		}
+	}
+	static void makeLower(std::string& url)
+	{
+		int nLen = (int)url.length();
+		char* pStr = (char*)url.c_str();
 
-        for (int i = 0; i < nLen; ++i)
-        {
-            if (pStr[i] >= 'A' && pStr[i] <= 'Z')
-                pStr[i] = pStr[i] + 'a' - 'A';
-        }
-    }
-    static void makeLowerW(std::wstring& url)
-    {
-        int nLen = (int)url.length();
-        wchar_t* pStr = (wchar_t*)url.c_str();
+		for (int i = 0; i < nLen; ++i)
+		{
+			if (pStr[i] >= 'A' && pStr[i] <= 'Z')
+				pStr[i] = pStr[i] + 'a' - 'A';
+		}
+	}
+	static void makeLowerW(std::wstring& url)
+	{
+		int nLen = (int)url.length();
+		wchar_t* pStr = (wchar_t*)url.c_str();
 
-        for (int i = 0; i < nLen; ++i)
-        {
-            if (pStr[i] >= 'A' && pStr[i] <= 'Z')
-                pStr[i] = pStr[i] + 'a' - 'A';
-        }
-    }
+		for (int i = 0; i < nLen; ++i)
+		{
+			if (pStr[i] >= 'A' && pStr[i] <= 'Z')
+				pStr[i] = pStr[i] + 'a' - 'A';
+		}
+	}
 
-    static std::wstring::size_type FindLowerCase(const std::wstring& string, const std::wstring& find)
-    {
-        std::wstring sTmp = string;
-        NSCommon::makeLowerW(sTmp);
-        return sTmp.find(find);
-    }
-    static std::wstring::size_type FindLowerCaseR(const std::wstring& string, const std::wstring& find)
-    {
-        std::wstring sTmp = string;
-        NSCommon::makeLowerW(sTmp);
-        return sTmp.rfind(find);
-    }
-    static std::string::size_type FindLowerCase(const std::string& string, const std::string& find)
-    {
-        std::string sTmp = string;
-        NSCommon::makeLower(sTmp);
-        return sTmp.find(find);
-    }
-    static std::string::size_type FindLowerCaseR(const std::string& string, const std::string& find)
-    {
-        std::string sTmp = string;
-        NSCommon::makeLower(sTmp);
-        return sTmp.rfind(find);
-    }
+	static std::wstring::size_type FindLowerCase(const std::wstring& string, const std::wstring& find)
+	{
+		std::wstring sTmp = string;
+		NSCommon::makeLowerW(sTmp);
+		return sTmp.find(find);
+	}
+	static std::wstring::size_type FindLowerCaseR(const std::wstring& string, const std::wstring& find)
+	{
+		std::wstring sTmp = string;
+		NSCommon::makeLowerW(sTmp);
+		return sTmp.rfind(find);
+	}
+	static std::string::size_type FindLowerCase(const std::string& string, const std::string& find)
+	{
+		std::string sTmp = string;
+		NSCommon::makeLower(sTmp);
+		return sTmp.find(find);
+	}
+	static std::string::size_type FindLowerCaseR(const std::string& string, const std::string& find)
+	{
+		std::string sTmp = string;
+		NSCommon::makeLower(sTmp);
+		return sTmp.rfind(find);
+	}
 
-    static void url_correct(std::wstring& url)
-    {
-        NSStringUtils::string_replace(url, L"/./", L"/");
+	static void url_correct(std::wstring& url)
+	{
+		NSStringUtils::string_replace(url, L"/./", L"/");
 
-        size_t posn = 0;
-        while (std::wstring::npos != (posn = url.find(L"/../")))
-        {
-            std::wstring::size_type pos2 = url.rfind(L"/", posn - 1);
+		size_t posn = 0;
+		while (std::wstring::npos != (posn = url.find(L"/../")))
+		{
+			std::wstring::size_type pos2 = url.rfind(L"/", posn - 1);
 
-            if (std::wstring::npos != pos2)
-            {
-                url.erase(pos2, posn - pos2 + 3);
-            }
-        }
+			if (std::wstring::npos != pos2)
+			{
+				url.erase(pos2, posn - pos2 + 3);
+			}
+		}
 
-        // MAC
-        if (0 == url.find(L"file:/") && 0 != url.find(L"file://"))
-        {
-            url.replace(0, 6, L"file:///");
-        }
-    }
+		// MAC
+		if (0 == url.find(L"file:/") && 0 != url.find(L"file://"))
+		{
+			url.replace(0, 6, L"file:///");
+		}
+	}
 
 	static std::string url_encode(const std::wstring& url)
 	{
@@ -152,28 +152,28 @@ namespace NSCommon
 			char c = urlA[i];
 
 			if ((c >= 'a' && c <= 'z') ||
-				(c >= 'A' && c <= 'Z') ||
-				(c >= '0' && c <= '9') ||
-				(c == ';') ||
-				(c == ',') ||
-				(c == '/') ||
-				(c == '?') ||
-				(c == ':') ||
-				(c == '@') ||
-				(c == '&') ||
-				(c == '=') ||
-				(c == '+') ||
-				(c == '$') ||
-				(c == '-') ||
-				(c == '_') ||
-				(c == '.') ||
-				(c == '!') ||
-				(c == '~') ||
-				(c == '*') ||
-				(c == '\'') ||
-				(c == '(') ||
-				(c == ')') ||
-				(c == '#'))
+					(c >= 'A' && c <= 'Z') ||
+					(c >= '0' && c <= '9') ||
+					(c == ';') ||
+					(c == ',') ||
+					(c == '/') ||
+					(c == '?') ||
+					(c == ':') ||
+					(c == '@') ||
+					(c == '&') ||
+					(c == '=') ||
+					(c == '+') ||
+					(c == '$') ||
+					(c == '-') ||
+					(c == '_') ||
+					(c == '.') ||
+					(c == '!') ||
+					(c == '~') ||
+					(c == '*') ||
+					(c == '\'') ||
+					(c == '(') ||
+					(c == ')') ||
+					(c == '#'))
 			{
 				result += c;
 			}
@@ -198,15 +198,15 @@ namespace NSCommon
 			char c = urlA[i];
 
 			if ((c >= 'a' && c <= 'z') ||
-				(c >= 'A' && c <= 'Z') ||
-				(c >= '0' && c <= '9') ||
-				(c == '/') ||
-				(c == '@') ||
-				(c == '+') ||
-				(c == '-') ||
-				(c == '_') ||
-				(c == '.') ||
-				(c == '~'))
+					(c >= 'A' && c <= 'Z') ||
+					(c >= '0' && c <= '9') ||
+					(c == '/') ||
+					(c == '@') ||
+					(c == '+') ||
+					(c == '-') ||
+					(c == '_') ||
+					(c == '.') ||
+					(c == '~'))
 			{
 				result += c;
 			}
@@ -248,247 +248,241 @@ namespace NSCommon
 class CFileDownloaderWrapper
 {
 private:
-    std::wstring m_sUrl;
-    std::wstring m_sOutput;
-    bool m_bIsDownloaded;
+	std::wstring m_sUrl;
+	std::wstring m_sOutput;
+	bool m_bIsDownloaded;
 
 public:
-    CFileDownloaderWrapper(const std::wstring& sUrl, const std::wstring& sOutput)
-    {
-        m_sUrl = sUrl;
-        m_sOutput = sOutput;
+	CFileDownloaderWrapper(const std::wstring& sUrl, const std::wstring& sOutput)
+	{
+		m_sUrl = sUrl;
+		m_sOutput = sOutput;
 
-        if (m_sOutput.empty())
-        {
-            m_sOutput = NSFile::CFileBinary::CreateTempFileWithUniqueName(NSDirectory::GetTempPath(), L"DW");
-            if (NSFile::CFileBinary::Exists(m_sOutput))
-                NSFile::CFileBinary::Remove(m_sOutput);
-        }
+		if (m_sOutput.empty())
+		{
+			m_sOutput = NSFile::CFileBinary::CreateTempFileWithUniqueName(NSDirectory::GetTempPath(), L"DW");
+			if (NSFile::CFileBinary::Exists(m_sOutput))
+				NSFile::CFileBinary::Remove(m_sOutput);
+		}
 
-        m_bIsDownloaded = false;
-    }
+		m_bIsDownloaded = false;
+	}
 
-    int DownloadSync()
-    {
-        NSNetwork::NSFileTransport::CFileDownloader oDownloader(m_sUrl, false);
-        oDownloader.SetFilePath(m_sOutput);
-        oDownloader.Start( 0 );
-        while ( oDownloader.IsRunned() )
-        {
-            NSThreads::Sleep( 10 );
-        }
-        m_bIsDownloaded = oDownloader.IsFileDownloaded();
-        return 0;
-    }
+	int DownloadSync()
+	{
+		NSNetwork::NSFileTransport::CFileDownloader oDownloader(m_sUrl, false);
+		oDownloader.SetFilePath(m_sOutput);
+		oDownloader.Start( 0 );
+		while ( oDownloader.IsRunned() )
+		{
+			NSThreads::Sleep( 10 );
+		}
+		m_bIsDownloaded = oDownloader.IsFileDownloaded();
+		return 0;
+	}
 
-    bool IsFileDownloaded()
-    {
-        return m_bIsDownloaded;
-    }
-    std::wstring GetFilePath()
-    {
-        return m_sOutput;
-    }
+	bool IsFileDownloaded()
+	{
+		return m_bIsDownloaded;
+	}
+	std::wstring GetFilePath()
+	{
+		return m_sOutput;
+	}
 };
 
 class CSystemVariablesMemory
 {
 private:
-    std::vector<char*> m_data;
+	std::vector<char*> m_data;
 
 public:
-    CSystemVariablesMemory()
-    {
-    }
-    ~CSystemVariablesMemory()
-    {
-        for (std::vector<char*>::iterator iter = m_data.begin(); iter != m_data.end(); iter++)
-        {
-            char* rem = *iter;
-            if (rem)
-                free(rem);
-        }
-        m_data.clear();
-    }
+	CSystemVariablesMemory()
+	{
+	}
+	~CSystemVariablesMemory()
+	{
+		for (std::vector<char*>::iterator iter = m_data.begin(); iter != m_data.end(); iter++)
+		{
+			char* rem = *iter;
+			if (rem)
+				free(rem);
+		}
+		m_data.clear();
+	}
 
-    char* Push(const std::string& str)
-    {
-        size_t str_len = (size_t)str.length();
-        char* sValueStr = (char*)malloc((str_len + 1) * sizeof(char));
-        memcpy(sValueStr, str.c_str(), str_len * sizeof(char));
-        sValueStr[str_len] = '\0';
-        m_data.push_back(sValueStr);
-        return sValueStr;
-    }
+	char* Push(const std::string& str)
+	{
+		size_t str_len = (size_t)str.length();
+		char* sValueStr = (char*)malloc((str_len + 1) * sizeof(char));
+		memcpy(sValueStr, str.c_str(), str_len * sizeof(char));
+		sValueStr[str_len] = '\0';
+		m_data.push_back(sValueStr);
+		return sValueStr;
+	}
 };
 
 namespace NSSystem
 {
-    static void SetEnvValueA(const std::string& sName, const std::string& sValue)
-    {
+	static void SetEnvValueA(const std::string& sName, const std::string& sValue)
+	{
 #ifdef WIN32
-        std::wstring sNameW = UTF8_TO_U(sName);
-        std::wstring sValueW = UTF8_TO_U(sValue);
-        SetEnvironmentVariable(sNameW.c_str(), sValueW.c_str());
+		std::wstring sNameW = UTF8_TO_U(sName);
+		std::wstring sValueW = UTF8_TO_U(sValue);
+		SetEnvironmentVariable(sNameW.c_str(), sValueW.c_str());
 #else
-        static char buffer[100000]; // on all process
-        static int offset = 0;
+		static char buffer[100000]; // on all process
+		static int offset = 0;
 
-        std::string tmp = sName + "=" + sValue;
-        size_t len = tmp.length();
+		std::string tmp = sName + "=" + sValue;
+		size_t len = tmp.length();
 
-        memcpy(buffer + offset, tmp.c_str(), sizeof(char) * len);
-        buffer[offset + len] = '\0';
-        putenv(buffer + offset);
-        offset += (len + 1);
+		memcpy(buffer + offset, tmp.c_str(), sizeof(char) * len);
+		buffer[offset + len] = '\0';
+		putenv(buffer + offset);
+		offset += (len + 1);
 #endif        
-    }
-    static void SetEnvValue(const std::string& sName, const std::wstring& sValue)
-    {
-        std::string sValueA = U_TO_UTF8(sValue);
-        return SetEnvValueA(sName, sValueA);
-    }
-    static std::string GetEnvValueA(const std::string& sName)
-    {
+	}
+	static void SetEnvValue(const std::string& sName, const std::wstring& sValue)
+	{
+		std::string sValueA = U_TO_UTF8(sValue);
+		return SetEnvValueA(sName, sValueA);
+	}
+	static std::string GetEnvValueA(const std::string& sName)
+	{
 #ifdef WIN32
-        std::wstring sNameW = UTF8_TO_U(sName);
-        const DWORD buffSize = 65535;
-        wchar_t buffer[buffSize];
-        if (GetEnvironmentVariable(sNameW.c_str(), buffer, buffSize))
-        {
-            std::wstring sValueW(buffer);
-            std::string sValue = U_TO_UTF8(sValueW);
-            return sValue;
-        }
-        else
-        {
-            return "";
-        }
+		std::wstring sNameW = UTF8_TO_U(sName);
+		const DWORD buffSize = 65535;
+		wchar_t buffer[buffSize];
+		if (GetEnvironmentVariable(sNameW.c_str(), buffer, buffSize))
+		{
+			std::wstring sValueW(buffer);
+			std::string sValue = U_TO_UTF8(sValueW);
+			return sValue;
+		}
+		else
+		{
+			return "";
+		}
 #else
-        char* pPath = getenv(sName.c_str());
-        if (NULL != pPath)
-            return std::string(pPath);
-        return "";
+		char* pPath = getenv(sName.c_str());
+		if (NULL != pPath)
+			return std::string(pPath);
+		return "";
 #endif
-    }
-    static std::wstring GetEnvValue(const std::string& sName)
-    {
-        std::string sValueA = GetEnvValueA(sName);
-        std::wstring sValue = UTF8_TO_U(sValueA);
-        return sValue;
-    }
+	}
+	static std::wstring GetEnvValue(const std::string& sName)
+	{
+		std::string sValueA = GetEnvValueA(sName);
+		std::wstring sValue = UTF8_TO_U(sValueA);
+		return sValue;
+	}
 }
 
 namespace NSUrlParse
 {
-    static std::wstring GetUrlValue(const std::wstring& sValue, const std::wstring& sProp)
-    {
-        std::wstring::size_type pos1 = sValue.find(sProp + L"=");
-        if (std::wstring::npos == pos1)
-            return L"";
+	static std::wstring GetUrlValue(const std::wstring& sValue, const std::wstring& sProp)
+	{
+		std::wstring::size_type pos1 = sValue.find(sProp + L"=");
+		if (std::wstring::npos == pos1)
+			return L"";
 
-        pos1 += (sProp.length() + 1);
+		pos1 += (sProp.length() + 1);
 
-        std::wstring::size_type pos2 = sValue.find(L"&", pos1);
-        if (std::wstring::npos == pos2)
-        {
-            return sValue.substr(pos1);
-        }
-        return sValue.substr(pos1, pos2  - pos1);
-    }
+		std::wstring::size_type pos2 = sValue.find(L"&", pos1);
+		if (std::wstring::npos == pos2)
+		{
+			return sValue.substr(pos1);
+		}
+		return sValue.substr(pos1, pos2  - pos1);
+	}
 }
 
 namespace NSFileDownloader
 {
-    static bool IsNeedDownload(const std::wstring& FilePath)
-    {
-        std::wstring::size_type n1 = FilePath.find(L"www.");
-        std::wstring::size_type n2 = FilePath.find(L"http://");
-        std::wstring::size_type n3 = FilePath.find(L"ftp://");
-        std::wstring::size_type n4 = FilePath.find(L"https://");
+	static bool IsNeedDownload(const std::wstring& FilePath)
+	{
+		std::wstring::size_type n1 = FilePath.find(L"www.");
+		std::wstring::size_type n2 = FilePath.find(L"http://");
+		std::wstring::size_type n3 = FilePath.find(L"ftp://");
+		std::wstring::size_type n4 = FilePath.find(L"https://");
 
-        if (n1 != std::wstring::npos && n1 < 10)
-            return true;
-        if (n2 != std::wstring::npos && n2 < 10)
-            return true;
-        if (n3 != std::wstring::npos && n3 < 10)
-            return true;
-        if (n4 != std::wstring::npos && n4 < 10)
-            return true;
+		if (n1 != std::wstring::npos && n1 < 10)
+			return true;
+		if (n2 != std::wstring::npos && n2 < 10)
+			return true;
+		if (n3 != std::wstring::npos && n3 < 10)
+			return true;
+		if (n4 != std::wstring::npos && n4 < 10)
+			return true;
 
-        return false;
-    }
+		return false;
+	}
 }
 
 class CJSONSimple
 {
 private:
-    NSStringUtils::CStringBuilder builder;
-    bool m_isSlash;
+	NSStringUtils::CStringBuilder builder;
 
 public:
-    CJSONSimple(bool isSlash = false)
-    {
-        m_isSlash = isSlash;
-    }
+	CJSONSimple()
+	{
+	}
+
+protected:
+	inline void WriteStringValue(const std::string& value)
+	{
+		WriteStringValue(UTF8_TO_U(value));
+	}
+	inline void WriteStringValue(const std::wstring& value)
+	{
+		std::wstring tmp = value;
+		NSStringUtils::string_replace(tmp, L"\\", L"\\\\");
+		NSStringUtils::string_replace(tmp, L"\"", L"\\\"");
+		builder.WriteString(tmp);
+	}
 
 public:
-    std::wstring GetData()
-    {
-        return builder.GetData();
-    }
+	std::wstring GetData()
+	{
+		return builder.GetData();
+	}
 
-    void Start()
-    {
-        builder.WriteString(L"{");
-    }
+	void Start()
+	{
+		builder.WriteString(L"{");
+	}
 
-    void End()
-    {
-        builder.WriteString(L"}");
-    }
+	void End()
+	{
+		builder.WriteString(L"}");
+	}
 
-    void Next()
-    {
-        builder.WriteString(L",");
-    }
+	void Next()
+	{
+		builder.WriteString(L",");
+	}
 
-    void Write(const std::wstring& name, const std::wstring& value)
-    {
-        m_isSlash ? builder.WriteString(L"\\\"") : builder.WriteString(L"\"");
-        builder.WriteString(name);
-        m_isSlash ? builder.WriteString(L"\\\":") : builder.WriteString(L"\":");
+	template<typename T>
+	void Write(const std::wstring& name, const T& value)
+	{
+		builder.WriteString(L"\"");
+		WriteStringValue(name);
+		builder.WriteString(L"\":");
 
-        std::wstring s = value;
-        if (m_isSlash)
-            NSStringUtils::string_replace(s, L"\"", L"\\\"");
+		builder.WriteString(L"\"");
+		WriteStringValue(value);
+		builder.WriteString(L"\"");
+	}
+	void Write(const std::wstring& name, const int& value)
+	{
+		builder.WriteString(L"\"");
+		WriteStringValue(name);
+		builder.WriteString(L"\":");
 
-        m_isSlash ? builder.WriteString(L"\\\"") : builder.WriteString(L"\"");
-        builder.WriteString(s);
-        m_isSlash ? builder.WriteString(L"\\\"") : builder.WriteString(L"\"");
-    }
-    void Write(const std::wstring& name, const std::string& value)
-    {
-        m_isSlash ? builder.WriteString(L"\\\"") : builder.WriteString(L"\"");
-        builder.WriteString(name);
-        m_isSlash ? builder.WriteString(L"\\\":") : builder.WriteString(L"\":");
-
-        std::wstring s = UTF8_TO_U(value);
-        if (m_isSlash)
-            NSStringUtils::string_replace(s, L"\"", L"\\\"");
-
-        m_isSlash ? builder.WriteString(L"\\\"") : builder.WriteString(L"\"");
-        builder.WriteString(s);
-        m_isSlash ? builder.WriteString(L"\\\"") : builder.WriteString(L"\"");
-    }
-    void Write(const std::wstring& name, const int& value)
-    {
-        m_isSlash ? builder.WriteString(L"\\\"") : builder.WriteString(L"\"");
-        builder.WriteString(name);
-        m_isSlash ? builder.WriteString(L"\\\":") : builder.WriteString(L"\":");
-
-        builder.AddInt(value);
-    }
+		builder.AddInt(value);
+	}
 };
 
 #ifdef _LINUX
@@ -496,18 +490,18 @@ public:
 #include <pwd.h>
 static std::wstring GetHomeDirectory()
 {
-    const char* sHome = std::getenv("home");
+	const char* sHome = std::getenv("home");
 
-    if (sHome == NULL)
-    {
-        sHome = getpwuid(getuid())->pw_dir;
-    }
+	if (sHome == NULL)
+	{
+		sHome = getpwuid(getuid())->pw_dir;
+	}
 
-    if (NULL == sHome)
-        return L"";
+	if (NULL == sHome)
+		return L"";
 
-    std::string temp = sHome;
-    return NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)temp.c_str(), (LONG)temp.length());
+	std::string temp = sHome;
+	return NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)temp.c_str(), (LONG)temp.length());
 }
 
 #endif
