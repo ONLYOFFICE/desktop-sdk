a599892e6bd54d4bf19d85b6011f978a35fc23a5
Timofey <timofey.boyko@onlyoffice.com>
2025-09-23 07:15:21 +0500

    Providers: add openai

--- ChromiumBasedEditors/plugins/ai-agent/src/providers/openai/index.ts
+++ ChromiumBasedEditors/plugins/ai-agent/src/providers/openai/index.ts
@@ -1,13 +1,25 @@
 import OpenAI from "openai";
 import type {
+  ChatCompletionChunk,
   ChatCompletionMessageParam,
+  ChatCompletionSystemMessageParam,
   ChatCompletionTool,
+  ChatCompletionToolMessageParam,
 } from "openai/resources/chat/completions";
+import cloneDeep from "lodash.clonedeep";
+import type { Model as OpenAIModel } from "openai/resources/models";
+import type { ThreadMessageLike } from "@assistant-ui/react";
 
-import type { Model, TProvider } from "@/lib/types";
+import type { Model, TMCPItem, TProvider } from "@/lib/types";
 
 import type { BaseProvider } from "../base";
-import type { SettingsProvider, TData } from "../settings";
+import type { SettingsProvider, TData, TErrorData } from "../settings";
+
+import {
+  convertToolsToModelFormat,
+  convertMessagesToModelFormat,
+} from "./utils";
+import { handleTextMessage, handleToolCall } from "./handlers";
 
 class OpenAIProvider
   implements
@@ -25,51 +37,257 @@ class OpenAIProvider
   tools: ChatCompletionTool[] = [];
   client?: OpenAI;
 
-  constructor() {
-    const client = new OpenAI({});
+  stopStream = false;
 
-    client.chat.completions.create();
+  constructor() {}
+
+  setProvider = (provider: TProvider) => {
+    this.provider = provider;
+
+    this.client = new OpenAI({
+      apiKey: provider.key,
+      baseURL: provider.baseUrl,
+      dangerouslyAllowBrowser: true,
+    });
+
+    if (provider.key) this.setApiKey(provider.key);
+    if (provider.baseUrl) this.setUrl(provider.baseUrl);
+  };
+
+  setModelKey = (modelKey: string) => {
+    this.modelKey = modelKey;
+  };
+
+  setSystemPrompt = (systemPrompt: string) => {
+    this.systemPrompt = systemPrompt;
+  };
+
+  setApiKey = (apiKey: string) => {
+    this.apiKey = apiKey;
+    if (this.client) this.client.apiKey = apiKey;
+  };
+
+  setUrl = (url: string) => {
+    this.url = url;
+    if (this.client) this.client.baseURL = url;
+  };
+
+  setPrevMessages = (prevMessages: ThreadMessageLike[]) => {
+    this.prevMessages = convertMessagesToModelFormat(prevMessages);
+  };
+
+  setTools = (tools: TMCPItem[]) => {
+    this.tools = convertToolsToModelFormat(tools);
+  };
+
+  async *sendMessage(
+    messages: ThreadMessageLike[],
+    afterToolCall?: boolean,
+    message?: ThreadMessageLike
+  ): AsyncGenerator<
+    ThreadMessageLike | { isEnd: true; responseMessage: ThreadMessageLike }
+  > {
+    try {
+      if (!this.client) return;
+
+      const convertedMessage = convertMessagesToModelFormat(messages);
+
+      const systemMessage: ChatCompletionSystemMessageParam = {
+        role: "system",
+        content: this.systemPrompt,
+      };
+
+      const stream = await this.client.chat.completions.create({
+        messages: [systemMessage, ...this.prevMessages, ...convertedMessage],
+        model: this.modelKey,
+        tools: this.tools,
+        stream: true,
+
+        max_tokens: 2048,
+      });
+
+      this.prevMessages.push(...convertedMessage);
+
+      let responseMessage: ThreadMessageLike =
+        afterToolCall && message
+          ? cloneDeep(message)
+          : {
+              role: "assistant",
+              content: [],
+            };
+
+      let stop = false;
+
+      for await (const messageStreamEvent of stream) {
+        if (this.stopStream) {
+          stream.controller.abort();
+
+          this.stopStream = false;
+
+          return;
+        }
+        const chunks: ChatCompletionChunk["choices"] =
+          messageStreamEvent.choices;
+
+        chunks.forEach((chunk) => {
+          if (this.stopStream) {
+            stream.controller.abort();
+
+            this.stopStream = false;
+
+            return;
+          }
+
+          if (stop) return;
+
+          if (chunk.finish_reason) {
+            stop = true;
+
+            const curMsg = afterToolCall
+              ? {
+                  ...responseMessage,
+                  content: responseMessage.content.slice(
+                    message?.content.length ?? 0
+                  ),
+                }
+              : responseMessage;
+
+            const providerMsg = convertMessagesToModelFormat([curMsg]);
+
+            this.prevMessages.push(...providerMsg);
+
+            return;
+          }
+
+          if (chunk.delta.content) {
+            responseMessage = handleTextMessage(
+              responseMessage,
+              chunk,
+              afterToolCall
+            );
+          }
+
+          if (
+            chunk.delta.tool_calls &&
+            typeof responseMessage.content !== "string"
+          ) {
+            responseMessage = handleToolCall(responseMessage, chunk);
+          }
+        });
+
+        if (stop) {
+          yield {
+            isEnd: true,
+            responseMessage,
+          };
+          continue;
+        } else {
+          yield responseMessage;
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      yield {
+        isEnd: true,
+        responseMessage: {
+          role: "assistant",
+          content: "",
+          status: {
+            type: "incomplete",
+            reason: "error",
+            error: e,
+          },
+        } as ThreadMessageLike,
+      };
+    }
   }
 
+  async *sendMessageAfterToolCall(
+    message: ThreadMessageLike
+  ): AsyncGenerator<
+    ThreadMessageLike | { isEnd: true; responseMessage: ThreadMessageLike }
+  > {
+    if (typeof message.content === "string") return message;
+
+    const result = message.content
+      .filter((c) => c.type === "tool-call")
+      .reverse()[0];
+
+    if (!result) return message;
+
+    const toolResult: ChatCompletionToolMessageParam = {
+      role: "tool",
+      content: result.result,
+      tool_call_id: result.toolCallId!,
+    };
+
+    this.prevMessages.push(toolResult);
+
+    yield* this.sendMessage([], true, message);
+
+    return message;
+  }
+
+  stopMessage = () => {
+    this.stopStream = true;
+  };
+
   getName = () => {
-    return "Ollama";
+    return "OpenAI";
   };
 
   getBaseUrl = () => {
-    return "http://localhost:11434";
+    return "https://api.openai.com/v1";
   };
 
   checkProvider = async (data: TData): Promise<boolean | TErrorData> => {
-    const checkClient = new Ollama({
-      host: data.url,
+    const checkClient = new OpenAI({
+      baseURL: data.url,
+      apiKey: data.apiKey,
+      dangerouslyAllowBrowser: true,
     });
 
     try {
-      await checkClient.list();
+      await checkClient.models.list();
 
       return true;
     } catch (error) {
       console.log(error);
+      const errorObj = error as { code: string };
 
-      return {
-        field: "url",
-        message: "Invalid URL",
-      };
+      if (errorObj.code === "invalid_api_key") {
+        return {
+          field: "key",
+          message: "Invalid API Key",
+        };
+      }
+
+      if (data.apiKey) {
+        return {
+          field: "url",
+          message: "Invalid URL",
+        };
+      }
     }
+    return {
+      field: "key",
+      message: "Empty key",
+    };
   };
 
   getProviderModels = async (data: TData): Promise<Model[]> => {
     const newClient = new OpenAI({
       baseURL: data.url,
       apiKey: data.apiKey,
+      dangerouslyAllowBrowser: true,
     });
 
-    const response: ListResponse = await newClient.models.list();
+    const response: OpenAIModel[] = (await newClient.models.list()).data;
 
-    return response.models.map((model) => ({
-      id: model.model,
-      name: model.name,
-      provider: "ollama" as const,
+    return response.map((model) => ({
+      id: model.id,
+      name: model.id,
+      provider: "openai" as const,
     }));
   };
 }
---
+++ ChromiumBasedEditors/plugins/ai-agent/src/providers/openai/utils.ts
@@ -0,0 +1,121 @@
+import type {
+  ChatCompletionMessageParam,
+  ChatCompletionTool,
+  ChatCompletionToolMessageParam,
+  ChatCompletionMessageFunctionToolCall,
+  ChatCompletionAssistantMessageParam,
+} from "openai/resources/chat/completions";
+import type { ThreadMessageLike } from "@assistant-ui/react";
+
+import type { TMCPItem } from "@/lib/types";
+
+export const convertToolsToModelFormat = (
+  tools: TMCPItem[]
+): ChatCompletionTool[] => {
+  return tools.map((tool) => ({
+    type: "function",
+    function: {
+      name: tool.name,
+      description: tool.description,
+      input_schema: {
+        type: "object",
+        ...tool.inputSchema,
+      },
+    },
+  }));
+};
+
+export const convertMessagesToModelFormat = (
+  messages: ThreadMessageLike[]
+): ChatCompletionMessageParam[] => {
+  const convertedMessages: ChatCompletionMessageParam[] = [];
+
+  messages.forEach((message) => {
+    if (message.role === "user" || message.role === "system") {
+      const content: ChatCompletionMessageParam["content"] =
+        typeof message.content === "string"
+          ? message.content
+          : message.content.map((part) => {
+              if (part.type === "text") {
+                return { type: "text", text: part.text };
+              }
+
+              if (part.type === "file") {
+                return {
+                  type: "file",
+                  file: {
+                    file_data: part.data,
+                    file_name: JSON.parse(part.mimeType).path,
+                  },
+                };
+              }
+
+              return { type: "text", text: "" };
+            });
+
+      //   if (message.attachments?.length) {
+      //     const imageContent = convertImageAttachmentsToContent(
+      //       message.attachments
+      //     );
+
+      //     if (Array.isArray(content)) {
+      //       content.push(...imageContent);
+      //     }
+      //   }
+
+      convertedMessages.push({
+        role: "user",
+        content,
+      });
+    } else {
+      const content: ChatCompletionAssistantMessageParam["content"] =
+        typeof message.content === "string" ? message.content : [];
+
+      const toolsResults: ChatCompletionToolMessageParam[] = [];
+      const toolsCalls: ChatCompletionMessageFunctionToolCall[] = [];
+
+      const msgContent = message.content;
+      if (typeof msgContent !== "string" && typeof content !== "string") {
+        msgContent.forEach((part) => {
+          if (!Array.isArray(content)) return;
+
+          if (part.type === "text") {
+            content.push({ type: "text", text: part.text });
+          }
+
+          if (part.type === "tool-call") {
+            if (part.result) {
+              toolsResults.push({
+                role: "tool",
+                content: part.result,
+                tool_call_id: part.toolCallId!,
+              });
+            }
+
+            toolsCalls.push({
+              id: part.toolCallId!,
+              type: "function",
+              function: { arguments: part.argsText ?? "", name: part.toolName },
+            });
+            return;
+          }
+        });
+      }
+
+      const msg: ChatCompletionAssistantMessageParam = {
+        role: "assistant",
+        content,
+      };
+
+      if (toolsCalls.length) msg.tool_calls = toolsCalls;
+
+      convertedMessages.push(msg);
+
+      if (toolsResults.length) {
+        convertedMessages.push(...toolsResults);
+      }
+    }
+  });
+
+  return convertedMessages;
+};
