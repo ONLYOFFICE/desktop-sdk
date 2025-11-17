d35d5f2f240d3f50ed5a530cf19c9bc2d61b6ec2
Timofey <timofey.boyko@onlyoffice.com>
2025-11-17 16:49:22 +0800

    Fixed Bug 78443 - Copy buttons and copy confirmation icon in a reply containing code are not adapted for the dark theme

--- ChromiumBasedEditors/plugins/ai-agent/src/components/markdown/sub-components/CodeHeader.tsx
+++ ChromiumBasedEditors/plugins/ai-agent/src/components/markdown/sub-components/CodeHeader.tsx
@@ -1,15 +1,18 @@
 import type { CodeHeaderProps } from "@assistant-ui/react-markdown";
-import { ReactSVG } from "react-svg";
+import { useTranslation } from "react-i18next";
 
 import BtnCopyIconUrl from "@/assets/btn-copy.svg?url";
 import BtnCheckIconUrl from "@/assets/checked.svg?url";
 
 import { TooltipIconButton } from "../../tooltip-icon-button";
+import { IconButton } from "../../icon-button";
 
 import useCopyToClipboard from "../hooks/useCopyToClipboard";
 
 const CodeHeader = ({ language, code }: CodeHeaderProps) => {
   const { isCopied, copyToClipboard } = useCopyToClipboard();
+
+  const { t } = useTranslation();
 
   const onCopy = () => {
     if (!code || isCopied) return;
@@ -19,11 +22,20 @@ const CodeHeader = ({ language, code }: CodeHeaderProps) => {
   return (
     <div className="mt-4 flex items-center justify-between gap-4 rounded-t-lg bg-[var(--chat-message-code-block-background-color)] text-[var(--chat-message-code-block-header-color)] text-[14px] leading-[20px] px-[12px] pt-[12px] pb-[8px]">
       <span className="lowercase">{language}</span>
-      <TooltipIconButton tooltip="Copy" onClick={onCopy} visible={!isCopied}>
-        {!isCopied && (
-          <ReactSVG className="cursor-pointer" src={BtnCopyIconUrl} />
+      <TooltipIconButton
+        tooltip={t("Copy")}
+        onClick={onCopy}
+        visible={!isCopied}
+      >
+        {!isCopied && <IconButton iconName={BtnCopyIconUrl} size={16} />}
+        {isCopied && (
+          <IconButton
+            iconName={BtnCheckIconUrl}
+            size={16}
+            isStroke
+            disableHover
+          />
         )}
-        {isCopied && <ReactSVG src={BtnCheckIconUrl} />}
       </TooltipIconButton>
     </div>
   );
